2d7aeff3e3774f20926bdb758c4d4621d38ba5a7
Casey Link <unnamedrambler@gmail.com>
2008-10-31 13:10:55 -0400

    Add barebones Artist type.

---
+++ src/com/binaryelysium/mp3tunes/Artist.java
@@ -0,0 +1,26 @@
+package com.binaryelysium.mp3tunes;
+
+public class Artist {
+	int mId;
+	String mName; 
+	int mTrackCount;
+	int mAlbumCount;
+	int mSize;
+
+	public int getId() {
+		return mId;
+	}
+	public String getName() {
+		return mName;
+	}
+	public int getTrackCount() {
+		return mTrackCount;
+	}
+	public int getAlbumCount() {
+		return mAlbumCount;
+	}
+	public int getSize() {
+		return mSize;
+	}
+	
+}
